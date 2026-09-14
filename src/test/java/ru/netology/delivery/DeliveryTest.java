a25c94060849163e50d580bbb1fb68ddc48708ca
Tatyanka <tatyana.kutsenko.84@mail.ru>
2026-09-14 21:06:44 +0300

    Add allure

--- src/test/java/ru/netology/delivery/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/DeliveryTest.java
@@ -1,9 +1,8 @@
 package ru.netology.delivery;
 
 import com.codeborne.selenide.Selectors;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -13,7 +12,14 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class DeliveryTest {
-
+@BeforeAll
+static void setUpAll() {
+    SelenideLogger.addListener("allure", new AllureSelenide());
+}
+ @AfterAll
+ static void tearDownAll() {
+    SelenideLogger.removeListener("allure");
+ }
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
